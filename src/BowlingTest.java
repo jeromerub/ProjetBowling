82b8dd48ed19c22a0da6a95a79fcbe570599a91f
jeromerub <ruby.jerome@hotmail.fr>
2016-04-15 10:19:53 +0200

    fichier test sur la brancheTest

---
+++ src/BowlingTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Utilisateur on 15/04/2016.
+ */
+public class BowlingTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void main() throws Exception {
+
+    }
+}
